24bca19ba847df03decc8c197ce619a97a5c78e3
Diana García <diana.eli.gg@gmail.com>
2020-09-27 22:47:21 -0500

    WIP: Adding floating icons

--- js/script.js
+++ js/script.js
@@ -7,61 +7,69 @@ const boops = {
 };
 const wrong = new Audio("assets/wrong.mp3");
 const sequence = [];
-const delay = 1 * 1000;
+const delay = 0.8 * 1000;
 const numBoopsAtStart = 3;
 let playerTurn = false;
 let currentColorIndex = 0;
+let currentScore = 0;
+let highScore = 0;
+let soundOn = true;
+let playing = false;
+
+document.getElementsByClassName("soundOff")[0].classList.add("hidden");
+document.getElementsByClassName("right")[0].classList.add("hidden");
+document.getElementsByClassName("wrong")[0].classList.add("hidden");
 
 const reset = () => {
   setTimeout(() => {
-    console.log("RESETTING");
     playerTurn = false;
     currentColorIndex = 0;
+    currentScore = 0;
+    updateScores();
     sequence.splice(0, sequence.length);
     for (i = 0; i < numBoopsAtStart; i++) {
       var r = Math.floor(Math.random() * options.length);
       sequence.push({ index: r, color: options[r] });
     }
-    console.log(sequence);
     showSequence(sequence, 0);
   }, delay);
 };
 
 const showSequence = (sequence, i) => {
-  if (i === 0) console.log('SHOW_SEQUENCE', sequence);
+  if (i === 0) console.log("SHOW_SEQUENCE", sequence);
   if (i < sequence.length) {
     showBoop(sequence[i].color);
     setTimeout(() => {
       showSequence(sequence, i + 1);
-    }, delay);
+    }, delay * 1.1);
   } else {
-    console.log('=============================');
     playerTurn = true;
   }
 };
 
 function showBoop(color, short = false) {
-  console.log("SHOW_BOOP", color);
-  boops[color].play();
+  playSound(boops[color]);
   document.querySelector(`.${color}`).classList.add(`${color}Highlight`);
-  setTimeout(() => {
-    document.querySelector(`.${color}`).classList.remove(`${color}Highlight`);
-  }, short ? delay/2 : delay);
+  setTimeout(
+    () => {
+      document.querySelector(`.${color}`).classList.remove(`${color}Highlight`);
+    },
+    short ? delay / 2 : delay
+  );
 }
 
-document.querySelectorAll(".button").forEach((element) => {
-  element.addEventListener("click", (e) => {
-    verifyClick(e.target.getAttribute('data-color'));
-  });
-});
-
 const verifyClick = (color) => {
-  // console.log("VERIFY_CLICK", color);
+  animateClick(true);
   if (playerTurn) {
     if (color === sequence[currentColorIndex].color) {
-      // console.log("CORRECT", color);
       showBoop(color, true);
+      if (currentScore < numBoopsAtStart) {
+        currentScore++;
+        updateScores();
+      }
       if (++currentColorIndex >= sequence.length) {
+        currentScore = sequence.length;
+        updateScores();
         playerTurn = false;
         currentColorIndex = 0;
         var r = Math.floor(Math.random() * options.length);
@@ -69,11 +77,69 @@ const verifyClick = (color) => {
         setTimeout(() => showSequence(sequence, 0), delay);
       }
     } else {
-      console.log("WRONG", color, sequence[currentColorIndex].color);
-      wrong.play();
+      playSound(wrong);
       reset();
     }
   }
 };
 
-reset();
+const animateClick = (x, y) => {
+  // const icon = right ? "right" : "wrong";
+  let floatingIcon = document.createElement("div");
+  floatingIcon.classList.add("floating");
+  floatingIcon.style.position = "fixed";
+  floatingIcon.style.left = x;
+  floatingIcon.style.top = y;
+  document.appendChild(floatingIcon);
+};
+
+const playSound = (sound) => {
+  if (soundOn) {
+    sound.play();
+  }
+};
+
+const updateScores = () => {
+  if (currentScore > highScore) {
+    highScore = currentScore;
+  }
+  document.getElementsByClassName(
+    "currentScoreValue"
+  )[0].textContent = currentScore;
+  document.getElementsByClassName("highScoreValue")[0].textContent = highScore;
+};
+
+document.querySelectorAll(".button").forEach((element) => {
+  element.addEventListener("click", (e) => {
+    verifyClick(e.target.getAttribute("data-color"));
+  });
+});
+
+const toggleSound = () => {
+  soundOn = !soundOn;
+  if (soundOn) {
+    document.getElementsByClassName("soundOn")[0].classList.add("visible");
+    document.getElementsByClassName("soundOff")[0].classList.add("hidden");
+    document.getElementsByClassName("soundOn")[0].classList.remove("hidden");
+    document.getElementsByClassName("soundOff")[0].classList.remove("visible");
+  } else {
+    document.getElementsByClassName("soundOn")[0].classList.add("hidden");
+    document.getElementsByClassName("soundOff")[0].classList.add("visible");
+    document.getElementsByClassName("soundOn")[0].classList.remove("visible");
+    document.getElementsByClassName("soundOff")[0].classList.remove("hidden");
+  }
+};
+
+document.querySelectorAll(".sounds").forEach((element) => {
+  element.addEventListener("click", (e) => {
+    toggleSound();
+  });
+});
+
+document.querySelectorAll(".statuses").forEach((element) => {
+  element.addEventListener("click", (e) => {
+    if (!playing) {
+      reset();
+    }
+  });
+});
